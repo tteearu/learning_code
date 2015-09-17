5727449e17b591c5774c3db12a13c84a6e6099e2
Marko Belzetski <marko.joh@gmail.com>
2015-09-17 17:34:28 +0300

    Updated complex solution

--- Python/Codeclub/complex/complex.py
+++ Python/Codeclub/complex/complex.py
@@ -1,6 +1,10 @@
 class Complex:
 	real = None
 	imaginary = None
+	a = None
+	b = None
+	c = None
+	d = None
 
 	def __init__(self,r,i):
 		self.real = float(r)
@@ -21,6 +25,16 @@ class Complex:
 	def __add__(self, other):
 		return Complex(self.real + other.real, self.imaginary + other.imaginary)
 
+	def __sub__(self,other):
+		return Complex(self.real - other.real, self.imaginary - other.imaginary)
 
+	def __mul__(self,other):
+		return Complex(((self.real * other.real) - (self.imaginary*other.imaginary)), ((self.real * other.imaginary) + (self.imaginary*other.real)))	
 
+	def __div__(self, other):
+		self.a = self.real
+		self.b = self.imaginary
+		self.c = other.real
+		self.d = other.imaginary
 
+		return Complex(((self.a*self.c)+(self.b*self.d))/((self.c*self.c)+(self.d*self.d)), ((self.b*self.c)-(self.a*self.d))/((self.c*self.c)+(self.d*self.d)))
