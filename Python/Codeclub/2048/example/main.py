c33a30e19a1a078983d477ff54a081c7fa5fd773
Marko Belzetski <marko.joh@gmail.com>
2015-09-27 17:10:14 +0300

    Adds semi-finished 2048 game solution

--- Python/Codeclub/2048/example/main.py
+++ Python/Codeclub/2048/example/main.py
@@ -2,32 +2,39 @@ from twozerofoureight import *
 
 game_board = board()
 	
-	
+game_board.generate_newpiece("start")
+game_board.generate_newpiece("start")
+game_board.print_board("normal")
 
-game_board.generate_newpiece()
-game_board.generate_newpiece()
-game_board.print_board()
+def move(direction):
+	original_board = list(game_board.board_list)
+	if direction == "left":
+		game_board.move_left_right("left")
+	elif direction == "right":
+		game_board.move_left_right("right")
+	elif direction == "up":
+		game_board.upkey()
+	elif direction == "down":
+		game_board.downkey()
+	else:
+		pass
+	if original_board == game_board.board_list:
+		game_board.generate_newpiece("checkforloss")
+	else:
+		game_board.generate_newpiece("normal")
 
 def play():
 	key = Arrow.input()
 	while key != Arrow.NONE:
 			# print(key)
 			if key == Arrow.UP:
-				game_board.upkey()
-				game_board.generate_newpiece()
-				game_board.print_board()
+				move("up")
 			elif key == Arrow.DOWN:
-				game_board.downkey()
-				game_board.generate_newpiece()
-				game_board.print_board()
+				move("down")
 			elif key == Arrow.LEFT:
-				game_board.leftkey()
-				game_board.generate_newpiece()
-				game_board.print_board()
+				move("left")				
 			elif key == Arrow.RIGHT:
-				game_board.rightkey()
-				game_board.generate_newpiece()
-				game_board.print_board()		
+				move("right")
 			key = Arrow.input()
 
 	print("None of the arrow keys was pressed")
--- Python/Codeclub/2048/example/twozerofoureight.py
+++ Python/Codeclub/2048/example/twozerofoureight.py
@@ -12,59 +12,79 @@ class board:
 		for i in range(0,4):
 			self.board_list.append([0,0,0,0])
 
-	def print_board(self):
+	def move_left_right(self,direction):
+		for n in range(len(self.board_list)):
+			self.board_list[n] = [x for x in self.board_list[n] if x != 0]
+			try:
+				for i in range(len(self.board_list[n])-1):
+					if self.board_list[n][i+1] == self.board_list[n][i]:
+						self.board_list[n][i] = self.board_list[n][i] * 2
+						del self.board_list[n][i+1]
+			except:
+				pass
+			if direction == "left":
+				while len(self.board_list[n]) != 4:
+					self.board_list[n].append(0)
+			elif direction == "right":
+				while len(self.board_list[n]) != 4:
+					self.board_list[n].insert(0,0)
+
+	def print_board(self, condition):
 		cls()
+		# print "\n"
 		for row in self.board_list:
 			print row
+		if condition == "win":
+			print "WIN"
+			quit()
+		elif condition == "lose":
+			print "LOSE"
+			quit()
+		else:
+			pass
+
+
+	def flip_board(self):
+		reversed_list = []
+		for row in self.board_list:
+			reversed_list.append([])
+		for row in self.board_list:
+			for n in range(len(self.board_list)):
+				reversed_list[n].append(row[n])
+		self.board_list = list(reversed_list)
 
 	def upkey(self):
-		# reversed_list = []
-		# 	for row in self.board_list:
-		# 		reversed_list.append([])
-
-		# 	for n in range(len(self.board_list)):
-		# 		reversed_list[n][n] = self.board_list[n][n]
-
-		pass
+		self.flip_board()
+		self.move_left_right("left")
+		self.flip_board()
 
 	def downkey(self):
-		# print "downkey"
-		pass
-	def leftkey(self):
-		for n in range(len(self.board_list)):
-			self.board_list[n] = [x for x in self.board_list[n] if x != 0]
-			try:
-				for i in range(len(self.board_list[n])-1):
-					if self.board_list[n][i+1] == self.board_list[n][i]:
-						self.board_list[n][i] = self.board_list[n][i] * 2
-						del self.board_list[n][i+1]
-			except:
-				pass
-			while len(self.board_list[n]) != 4:
-				self.board_list[n].append(0)
+		self.flip_board()
+		self.move_left_right("right")
+		self.flip_board()
 
-	def rightkey(self):
-		for n in range(len(self.board_list)):
-			self.board_list[n] = [x for x in self.board_list[n] if x != 0]
-			try:
-				for i in range(len(self.board_list[n])-1):
-					if self.board_list[n][i+1] == self.board_list[n][i]:
-						self.board_list[n][i] = self.board_list[n][i] * 2
-						del self.board_list[n][i+1]
-			except:
-				pass
-			while len(self.board_list[n]) != 4:
-				self.board_list[n].insert(0,0)
-
-	def generate_newpiece(self):
+	def generate_newpiece(self, condition):
 		x = randrange(0,4)
 		y = randrange(0,4)
 		newvalue = randrange(2,6,2)
 
-		while self.board_list[x][y] != 0:
-			x = randrange(0,4)
-			y = randrange(0,4)
-		else:
-			self.board_list[x][y] = newvalue
+		zeroes = 0
+		for row in self.board_list:
+			zeroes += row.count(0)
 
-	# def moveleft(self):
+		win = 0
+		for row in self.board_list:
+			win += row.count(2048)
+
+		if zeroes > 0 or condition == "start":
+			while self.board_list[x][y] != 0:
+				x = randrange(0,4)
+				y = randrange(0,4)
+			else:
+				self.board_list[x][y] = newvalue
+
+			self.print_board("normal")
+		elif win > 0:
+			self.print_board("win")
+		elif zeroes == 0 and condition == "checkforloss":
+			self.print_board("lose")
